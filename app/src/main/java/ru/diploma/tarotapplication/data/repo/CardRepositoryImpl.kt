0b467f9db15c11b9c62989031369575cc0c15182
geykerannam <geykerannam@gmail.com>
2023-11-07 23:00:51 +0300

    feat: добавлены пустые файлы с данными карт

--- app/src/main/java/ru/diploma/tarotapplication/data/repo/CardRepositoryImpl.kt
+++ app/src/main/java/ru/diploma/tarotapplication/data/repo/CardRepositoryImpl.kt
@@ -9,14 +9,21 @@ import ru.diploma.tarotapplication.data.model.InfoCard
 import ru.diploma.tarotapplication.data.model.Tags
 import ru.diploma.tarotapplication.data.model.TarotCardCollection
 import ru.diploma.tarotapplication.domain.repositories.CardRepository
+import java.io.IOException
 import javax.inject.Inject
 
 class CardRepositoryImpl @Inject constructor() : CardRepository {
 
     override suspend fun getItemByID(id: Long): Card {
-        val jsonFileString = getJsonDataFromAsset(id.toString())
-        val gson = Gson()
-        val listCardType = object : TypeToken<Card>() {}.type
-        return gson.fromJson(jsonFileString, listCardType)
+        var card: Card = Card.shimmerData
+        try {
+            val jsonFileString = getJsonDataFromAsset(id.toString())
+            val gson = Gson()
+            val listCardType = object : TypeToken<Card>() {}.type
+            card = gson.fromJson(jsonFileString, listCardType)
+        } catch (ioException: IOException) {
+            ioException.printStackTrace()
+        }
+        return card
     }
 }
